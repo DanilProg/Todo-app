334dc59ea6f0e66c92b5776ec70031fc78da9d20
danil <danil_12345677@mail.ru>
2024-09-23 18:04:36 +0700

    create add todo, edit todo, delete todo

---
+++ src/componennts/TodosList/TodoList.tsx
@@ -0,0 +1,32 @@
+import { Todo } from "../Todo/Todo.tsx";
+import { Box } from "@chakra-ui/react";
+import { ITodo } from "../../App.tsx";
+interface TodoListProps {
+  todos: ITodo[];
+  deleteTodo: (id: string) => void;
+  onEdit: (id: string, value: string) => void;
+}
+
+export const TodoList = ({ todos, deleteTodo, onEdit }: TodoListProps) => {
+  return (
+    <Box bg="white" p={"18px"} borderRadius={"6px"}>
+      {todos.map((todo) => {
+        const onStartEdit = () => {
+          onEdit(todo.id, todo.value);
+        };
+        const onDelete = () => {
+          deleteTodo(todo.id);
+        };
+        return (
+          <Todo
+            value={todo.value}
+            key={todo.id}
+            onDelete={onDelete}
+            onEdit={onStartEdit}
+            id={todo.id}
+          />
+        );
+      })}
+    </Box>
+  );
+};
